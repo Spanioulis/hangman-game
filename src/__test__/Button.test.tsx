24b6c9ea159d4e9982b475b30e1f25534cd555fb
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-03-18 23:01:09 +0100

    add new test to Button

--- src/__test__/Button.test.tsx
+++ src/__test__/Button.test.tsx
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { vi } from 'vitest';
 import Button from '../components/Button';
+import { colors } from '../styles';
 
 describe('Button', () => {
    const onClick = vi.fn();
@@ -14,12 +15,45 @@ describe('Button', () => {
       );
    });
 
-   it('renders correctly', () => {
-      expect(screen.getByText('Testing Button')).toBeInTheDocument();
+   it('renders content', () => {
+      const button = screen.getByText('Testing Button');
+      expect(button).toBeInTheDocument();
    });
 
-   it('correct button...', () => {
+   it('correct role', () => {
       const button = screen.getByRole('button', { name: /testing button/i });
       expect(button);
+   });
+
+   it('calls onClick prop when clicked', () => {
+      fireEvent.click(screen.getByText(/button/i));
+      expect(onClick).toHaveBeenCalledTimes(1);
+   });
+
+   // it('change the style when you hover over the button', async () => {
+   //    const button = screen.getByRole('button', { name: /testing button/i });
+
+   //    expect(button).toHaveStyle(`
+   //    background-color: ${colors.button.background};
+   //    color: ${colors.button.text};
+   //    `);
+
+   //    fireEvent.mouseOver(button);
+
+   //    await waitFor(() => {
+   //       expect(button).toHaveStyle(`
+   //          background-color: #e7a127;
+   //          color: #824d30;
+   //       `);
+   //    });
+   // });
+
+   it('renders with correct styles', () => {
+      const button = screen.getByText('Testing Button');
+      expect(button).toHaveStyle('cursor: pointer;');
+      expect(button).toHaveStyle(`background-color: ${colors.button.background};`);
+      expect(button).toHaveStyle('border-radius: 10px;');
+      expect(button).toHaveStyle('transition: all 0.3s ease-in-out;');
+      expect(button).toHaveStyle('width: 3rem;');
    });
 });
